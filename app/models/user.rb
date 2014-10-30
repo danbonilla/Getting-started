a5fb4db8e9a49861d34e8127fd8b58021a878540
danbonilla <dbonilla11@gmail.com>
2014-10-30 19:57:28 -0400

    adding session helpers
    remember me token, session helpers, and models were all updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # this is an email REGEX (regular expression)
@@ -13,5 +14,27 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # This creates and returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #remembers the user in the database to use in future sessions
+  def remember
+    self.remember_token = User.new_token 
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #if the given token matches the digest, it returns true if authenticated
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  #this forgets a user 
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
